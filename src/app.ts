d8da786e5b4b592659e1dfeeaccfc2c699c739ab
kanta1207 <kan0712livepool@gmail.com>
2023-08-26 14:49:29 -0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import * as bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { AppDataSource } from "./data-source";
+import { Routes } from "./routes";
+import { User } from "./entity/User";
+
+AppDataSource.initialize()
+  .then(async () => {
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
+
+    // register express routes from defined application routes
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: Function) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
+    });
+
+    // start express server
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Express server has started on port 3000. Open http://localhost:${process.env.PORT}/users to see results`
+      );
+    });
+  })
+  .catch((error) => console.log(error));
