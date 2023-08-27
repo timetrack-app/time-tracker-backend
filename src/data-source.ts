1a750dedf9b649361c7f950b6ce943d527727ce9
kanta1207 <kan0712livepool@gmail.com>
2023-08-26 18:31:57 -0700

    fix: port number

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,7 +9,7 @@ config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
+  port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
