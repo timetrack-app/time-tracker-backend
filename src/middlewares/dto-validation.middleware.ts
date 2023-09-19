8202105fe50b47dc6f8b69b9cf3affc605bdd6ff
koyaaoyama <aoyama.ko0920@gmail.com>
2023-09-19 13:55:02 -0700

    Add comments in dto-validation.middleware

--- src/middlewares/dto-validation.middleware.ts
+++ src/middlewares/dto-validation.middleware.ts
@@ -2,6 +2,29 @@ import { Request, Response, NextFunction } from "express";
 import { plainToClass } from 'class-transformer';
 import { validate, ValidationError } from "class-validator";
 
+/**
+ * Validate Dto class
+ * Used for request validation in controllers
+ *
+ * Usage with inversify-express-utils decorator:
+ * @httpPost('/login', DtoValidationMiddleware(AuthLoginDto))
+ *
+ * Error json structure:
+ * {
+      "statusCode": 400,
+      "success": false,
+      "message": "",
+      "error": {
+        "property1": ["error_message_1"],
+        "property2": ["error_message_2"],
+        // Additional properties and error messages if there are more validation errors
+      }
+    }
+ *
+ * @param {*} type
+ * @param {boolean} [skipMissingProperties=false]
+ * @return {*}
+ */
 export const DtoValidationMiddleware = (type: any, skipMissingProperties = false) => {
     return (req: Request, res: Response, next: NextFunction) => {
         const dtoObj = plainToClass(type, req.body);
@@ -12,7 +35,7 @@ export const DtoValidationMiddleware = (type: any, skipMissingProperties = false
                     errors.forEach(err => {
                         errMsg[err.property] = [...(Object as any).values(err.constraints)]
                     });
-                    
+
                     res.status(400).json({
                         statusCode: 400,
                         success: false,
--- src/modules/workSession/interfaces/IWorkSession.service.ts
+++ src/modules/workSession/interfaces/IWorkSession.service.ts
@@ -5,4 +5,5 @@ import { UpdateWorkSessionDto } from "../dto/update-work-session-dto";
 export interface IWorkSessionService {
   createWorkSession(createWorkSessionServiceDto: CreateWorkSessionServiceDto): Promise<WorkSession>;
   updateWorkSession(updateWorkSessionDto: UpdateWorkSessionDto): Promise<WorkSession>;
+  // TODO: getLatestWorkSession
 };
