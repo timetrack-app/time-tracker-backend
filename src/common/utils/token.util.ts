ff13eb5452f6307644124c28ed5369ff34fdd694
koyaaoyama <aoyama.ko0920@gmail.com>
2023-10-21 16:28:54 -0700

    Add isTokenUnexpired() to token.util

--- src/common/utils/token.util.ts
+++ src/common/utils/token.util.ts
@@ -1,5 +1,8 @@
 import crypto from 'crypto';
 
+// 1 hour
+export const tokenLifetimeMilliSec = 60 * 60 * 1000;
+
 /**
  * Create token
  *
@@ -7,4 +10,17 @@ import crypto from 'crypto';
  */
 export const createToken = async (): Promise<string> => {
   return await crypto.randomBytes(32).toString('hex');
+};
+
+/**
+ * Check if a token is expired or not
+ *
+ * @param {Date} tokenCreatedAt
+ * @return {boolean} true if token is not expired
+ */
+export const isTokenUnexpired = (tokenCreatedAt: Date): boolean => {
+  const now = new Date();
+  const lifeTimeMilliSecAgo = new Date(now.getTime() - tokenLifetimeMilliSec);
+
+  return tokenCreatedAt >= lifeTimeMilliSecAgo && tokenCreatedAt <= now;
 };
