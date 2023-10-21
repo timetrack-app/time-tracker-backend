00795cc321ace53181b741a05ad581331a49e8cf
koyaaoyama <aoyama.ko0920@gmail.com>
2023-10-20 17:14:32 -0700

    Implement AuthGuardMiddleware. Fix passport service

--- src/modules/passport/service/passport.service.ts
+++ src/modules/passport/service/passport.service.ts
@@ -1,41 +1,39 @@
 import passport from 'passport';
 import { inject, injectable } from 'inversify';
-import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
+import { ExtractJwt, Strategy as JwtStrategy, StrategyOptions, VerifyCallback } from 'passport-jwt';
 import { TYPES } from '../../../core/type.core';
-
 import { IUserService } from '../../user/interfaces/IUser.service';
 import { IPassportService } from '../interface/IPassport.service';
+import { getJwtSecret } from '../../../common/utils/env.utils';
 
 @injectable()
 export class PassportService implements IPassportService {
   constructor(@inject(TYPES.IUserService) private userService: IUserService) {}
 
   public init() {
-    passport.use(
-      'jwt',
-      new JwtStrategy(
-        {
-          jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-          secretOrKey: process.env.JWT_SECRET_KEY,
-        },
-        async (jwtPayload, done) => {
-          try {
-            // Find the user by ID from the JWT payload
-            const user = await this.userService.findOneById(jwtPayload.id);
+    const opts: StrategyOptions = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: getJwtSecret(),
+    };
 
-            // If the user doesn't exist, return an error
-            if (!user) {
-              return done(null, false);
-            }
+    const verifyCallback: VerifyCallback = async (jwtPayload, done) => {
+      try {
+        // Find the user by ID from the JWT payload
+        const user = await this.userService.findOneById(jwtPayload.id);
 
-            // If the user exists, return the user
-            return done(null, user);
-          } catch (error) {
-            return done(error);
-          }
-        },
-      ),
-    );
+        // If the user doesn't exist, return an error
+        if (!user) {
+          return done(null, false);
+        }
+
+        // If the user exists, return the user
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+
+    passport.use(new JwtStrategy(opts, verifyCallback));
   }
 
   jwtAuthenticate() {
